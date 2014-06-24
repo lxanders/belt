c0dc2596475a1fcc8bac2968f95cef95ad43d632
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-06-24 17:52:43 +0200

    added startsWith functionality

--- test/unit/lib/stringUtilSpec.js
+++ test/unit/lib/stringUtilSpec.js
@@ -106,6 +106,54 @@
 
         });
 
+        describe('startsWith', function () {
+
+            var startsWithTestCases = [
+                    { stringToCheck: 'a', stringToSearchFor: 'a' },
+                    { stringToCheck: 'any', stringToSearchFor: 'any' },
+                    { stringToCheck: ' any', stringToSearchFor: ' any' },
+                    { stringToCheck: 'any string', stringToSearchFor: 'any' },
+                    { stringToCheck: 'anything', stringToSearchFor: 'any' },
+                    { stringToCheck: 'this is quite some sentence', stringToSearchFor: 'this is quite' }
+                ],
+                doesntStartWithTestCases = [
+                    { stringToCheck: '  any', stringToSearchFor: 'any' },
+                    { stringToCheck: 'the same any', stringToSearchFor: 'any' },
+                    { stringToCheck: 'the same any', stringToSearchFor: ' any' },
+                    { stringToCheck: 'any', stringToSearchFor: 'anyString' },
+                    { stringToCheck: 'any', stringToSearchFor: 'a n y' },
+                    { stringToCheck: 'any', stringToSearchFor: '  any' },
+                    { stringToCheck: 'any', stringToSearchFor: 'any ' },
+                    { stringToCheck: undefined, stringToSearchFor: undefined },
+                    { stringToCheck: undefined, stringToSearchFor: null },
+                    { stringToCheck: undefined, stringToSearchFor: '' },
+                    { stringToCheck: undefined, stringToSearchFor: 'any' },
+                    { stringToCheck: null, stringToSearchFor: undefined },
+                    { stringToCheck: null, stringToSearchFor: null },
+                    { stringToCheck: null, stringToSearchFor: '' },
+                    { stringToCheck: null, stringToSearchFor: 'any' },
+                    { stringToCheck: 'any', stringToSearchFor: undefined },
+                    { stringToCheck: 'any', stringToSearchFor: null },
+                    { stringToCheck: 'any', stringToSearchFor: '' },
+                    { stringToCheck: 'any', stringToSearchFor: ' ' },
+                    { stringToCheck: '', stringToSearchFor: '' },
+                    { stringToCheck: ' ', stringToSearchFor: '' }
+                ];
+
+            startsWithTestCases.forEach(function (testCase) {
+                it('should return true for "' + testCase.stringToCheck + '" contains "' + testCase.stringToSearchFor + '" if ignoreCase = ' + testCase.ignoreCase, function () {
+                    expect(stringUtil.startsWith(testCase.stringToCheck, testCase.stringToSearchFor, testCase.ignoreCase)).to.be.equal(true);
+                });
+            });
+
+            doesntStartWithTestCases.forEach(function (testCase) {
+                it('should return false for "' + testCase.stringToCheck + '" contains "' + testCase.stringToSearchFor + '" if ignoreCase = ' + testCase.ignoreCase, function () {
+                    expect(stringUtil.startsWith(testCase.stringToCheck, testCase.stringToSearchFor, testCase.ignoreCase)).to.not.equal(true);
+                });
+            });
+
+        });
+
     });
 
 }());
